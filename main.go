073fbd7a68bc8b5dff603e91a3f101f1d1c14d62
sredny <sredny.nevermind92@gmail.com>
2017-05-07 17:01:10 -0400

    connected to DB and changed port

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
-import (
-    "strconv"   
+import ( 
      "github.com/gin-gonic/gin"
+	  "github.com/jinzhu/gorm"
+	  _ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type Users struct {
@@ -12,44 +13,142 @@ type Users struct {
 }
 
 func PostUser(c *gin.Context) {
-    // The futur code
+     db := InitDb()
+	   defer db.Close()
+	var user Users
+    c.Bind(&user)
+	
+	if user.Firstname != "" && user.Lastname != "" {
+        // INSERT INTO "users" (name) VALUES (user.Name);
+        db.Create(&user)
+        // Display error
+        c.JSON(201, gin.H{"success": user})
+    } else {
+        // Display error
+        c.JSON(422, gin.H{"error": "Fields are empty"})
+    }
+	 // curl -i -X POST -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Queen\" }" http://localhost:8080/api/v1/users
 }
 
 func GetUsers(c *gin.Context) {
-    var users = []Users{
-        Users{Id: 1, Firstname: "Oliver", Lastname: "Queen"},
-        Users{Id: 2, Firstname: "Malcom", Lastname: "Merlyn"},
-    }
+    // Connection to the database
+    db := InitDb()
+    // Close connection database
+    defer db.Close()
 
+    var users []Users
+    // SELECT * FROM users
+    db.Find(&users)
+
+    // Display JSON result
     c.JSON(200, users)
 
-    // curl -i http://localhost:8080/api/v1/users
+    // curl -i http://localhost:10000/api/v1/users
 }
 
 func GetUser(c *gin.Context) {
-    id := c.Params.ByName("id")
-    user_id, _ := strconv.ParseInt(id, 0, 64)
+   // Connection to the database
+    db := InitDb()
+    // Close connection database
+    defer db.Close()
 
-    if user_id == 1 {
-        content := gin.H{"id": user_id, "firstname": "Oliver", "lastname": "Queen"}
-        c.JSON(200, content)
-    } else if user_id == 2 {
-        content := gin.H{"id": user_id, "firstname": "Malcom", "lastname": "Merlyn"}
-        c.JSON(200, content)
+    id := c.Params.ByName("id")
+    var user Users
+    // SELECT * FROM users WHERE id = 1;
+    db.First(&user, id)
+
+    if user.Id != 0 {
+        // Display JSON result
+        c.JSON(200, user)
     } else {
-        content := gin.H{"error": "user with id#" + id + " not found"}
-        c.JSON(404, content)
+        // Display JSON error
+        c.JSON(404, gin.H{"error": "User not found"})
     }
 
-    // curl -i http://localhost:8080/api/v1/users/1
+    // curl -i http://localhost:10000/api/v1/users/1
 }
 
 func UpdateUser(c *gin.Context) {
-    // The futur cod
+     // Connection to the database
+    db := InitDb()
+    // Close connection database
+    defer db.Close()
+
+    // Get id user
+    id := c.Params.ByName("id")
+    var user Users
+    // SELECT * FROM users WHERE id = 1;
+    db.First(&user, id)
+
+    if user.Firstname != "" && user.Lastname != "" {
+
+        if user.Id != 0 {
+            var newUser Users
+            c.Bind(&newUser)
+
+            result := Users{
+                Id:        user.Id,
+                Firstname: newUser.Firstname,
+                Lastname:  newUser.Lastname,
+            }
+
+            // UPDATE users SET firstname='newUser.Firstname', lastname='newUser.Lastname' WHERE id = user.Id;
+            db.Save(&result)
+            // Display modified data in JSON message "success"
+            c.JSON(200, gin.H{"success": result})
+        } else {
+            // Display JSON error
+            c.JSON(404, gin.H{"error": "User not found"})
+        }
+
+    } else {
+        // Display JSON error
+        c.JSON(422, gin.H{"error": "Fields are empty"})
+    }
+
+    // curl -i -X PUT -H "Content-Type: application/json" -d "{ \"firstname\": \"Thea\", \"lastname\": \"Merlyn\" }" http://localhost:10000/api/v1/users/1
 }
 
 func DeleteUser(c *gin.Context) {
-    // The futur code
+     // Connection to the database
+    db := InitDb()
+    // Close connection database
+    defer db.Close()
+
+    // Get id user
+    id := c.Params.ByName("id")
+    var user Users
+    // SELECT * FROM users WHERE id = 1;
+    db.First(&user, id)
+
+    if user.Id != 0 {
+        // DELETE FROM users WHERE id = user.Id
+        db.Delete(&user)
+        // Display JSON result
+        c.JSON(200, gin.H{"success": "User #" + id + " deleted"})
+    } else {
+        // Display JSON error
+        c.JSON(404, gin.H{"error": "User not found"})
+    }
+
+    // curl -i -X DELETE http://localhost:1000/api/v1/users/1
+}
+
+func InitDb() *gorm.DB {
+    
+	db, err := gorm.Open("postgres", "host=199.168.136.134 user=mlsvowuser dbname=test_api sslmode=disable password=0112358")
+  
+    db.LogMode(true)
+	
+    // Error
+    if err != nil {
+        panic(err)
+    }
+
+    // Creating the table
+    db.AutoMigrate(&Users{})
+
+    return db
 }
 
 func main() {
@@ -64,5 +163,5 @@ func main() {
         v1.DELETE("/users/:id", DeleteUser)
     }
 
-    r.Run(":8080")
+    r.Run(":10000")
 }
